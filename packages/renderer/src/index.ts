f1896edb96da5a2035d9033ea5c33091e0394a4f
Tovi <1018496570@qq.com>
2022-09-01 14:09:43 +0800

    a

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -2,10 +2,10 @@
 // import App from '/@/App.vue';
 
 // createApp(App).mount('#app');
-
 import { ViteSSG } from 'vite-ssg'
 import generatedRoutes from 'virtual:generated-pages'
 import { setupLayouts } from 'virtual:generated-layouts'
+import type { UserModule } from './types'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
@@ -20,6 +20,7 @@ export const createApp = ViteSSG(
   { routes, base: import.meta.env.BASE_URL },
   (ctx) => {
     // install all modules under `modules/`
-    Object.values(import.meta.globEager('./modules/*.ts')).forEach(i => i.install?.(ctx))
+    Object.values(import.meta.glob<{ install: UserModule }>('./modules/*.ts', { eager: true }))
+      .forEach(i => i.install?.(ctx))
   },
 )
