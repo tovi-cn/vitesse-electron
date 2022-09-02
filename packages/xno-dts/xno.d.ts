810465ed74d164f8d0c3fc76902e978adb0c5c36
Tovi <1018496570@qq.com>
2022-09-02 18:47:24 +0800

    dts

--- packages/xno-dts/xno.d.ts
+++ packages/xno-dts/xno.d.ts
@@ -1,1 +1,27 @@
-declare module 'xno' {}
+declare module 'xno' {
+
+  export const version: string;
+
+  export interface Command {
+		/**
+		 * 命令的标题，例如 `save`.
+		 */
+		title: string;
+
+		/**
+		 * 实际命令处理程序的标识符.
+		 * @see {@link commands.registerCommand}
+		 */
+		command: string;
+
+		/**
+		 * 命令的工具提示，在 UI 中表示时.
+		 */
+		tooltip?: string;
+
+		/**
+		 * 命令处理程序应该被调用的参数
+		 */
+		arguments?: any[];
+	}
+}
