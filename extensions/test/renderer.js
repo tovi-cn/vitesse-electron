8ab69040c264a5b1f649d9ce3f8b5059fb467259
Tovi <1018496570@qq.com>
2022-09-02 17:16:08 +0800

    a

--- extensions/test/renderer.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line no-undef
-const information = document.getElementById('info');
-// eslint-disable-next-line no-undef
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
